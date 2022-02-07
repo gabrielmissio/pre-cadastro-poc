ccd12cf35ed39c944dbe819323d8fb48835d61d1
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-02-07 17:43:40 -0300

    feat: add jest to devDependencies and create config file

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.spec.js', '**/__tests__/**/*.test.js'],
+  testPathIgnorePatterns: ['/node_modules/']
+};
