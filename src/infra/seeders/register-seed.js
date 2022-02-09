267a8ec9d70c862148cfe42d514011dca08614f1
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-09 11:16:32 -0300

    feat(seeders): create populateTable and deleteTableItems methods in register-seed

--- src/infra/seeders/register-seed.js
+++ src/infra/seeders/register-seed.js
@@ -1,25 +1,49 @@
 const AWS = require('aws-sdk');
+const data = require('./user-seed.json');
 
 AWS.config.update({
   region: 'localhost',
   endpoint: 'http://localhost:8000'
 });
 
-const dynamodb = new AWS.DynamoDB();
-
-const data = require('./user-seed.json');
+const dynamoDocClient = new AWS.DynamoDB.DocumentClient();
 
 const TABLE_NAME = data.tableName;
 
-const populateDB = () => {
+const populateTable = () => {
   data.items.forEach(async (item) => {
-    await dynamodb
-      .putItem({
+    await dynamoDocClient
+      .put({
         TableName: TABLE_NAME,
         Item: item
       })
       .promise();
   });
 
-  console.log('The table has been populated');
+  console.log(`The table ${TABLE_NAME} has been populated`);
 };
+
+const deleteTableItems = () => {
+  data.items.forEach(async (item) => {
+    await dynamoDocClient
+      .delete({
+        TableName: TABLE_NAME,
+        Key: {
+          PK: item.PK,
+          SK: item.SK
+        }
+      })
+      .promise();
+  });
+
+  console.log(`The items from table ${TABLE_NAME} has been removed`);
+};
+
+const handler = () => {
+  const operation = process.argv[2];
+  if (operation === 'populate') return populateTable();
+
+  return deleteTableItems();
+};
+
+handler();
