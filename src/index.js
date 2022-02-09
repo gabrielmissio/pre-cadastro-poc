3dea1c91644772c9b1538b4b6047d2f6255993a4
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-09 16:42:08 -0300

    feat: set errors and routes methods in App class

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const routes = require('./routes');
+const handler = require('./app/middlewares/errorMiddleware');
 
 class App {
   constructor() {
     this.express = express();
     this.middlewares();
+    this.routes();
+    this.errors();
   }
 
   middlewares() {
     this.express.use(express.json());
+  }
+
+  routes() {
+    this.express.use(routes);
+  }
+
+  errors() {
+    this.express.use(handler);
   }
 }
 
