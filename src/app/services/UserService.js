6a10c4ff3a1f6c294343347774015e85eecfdb19
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-10 17:07:57 -0300

    feat(services): create getById method of UserService

--- src/app/services/UserService.js
+++ src/app/services/UserService.js
@@ -1,8 +1,15 @@
 const UserRepository = require('../repositorys/UserRepository');
+const PKConcatenator = require('../utils/PK-concatenator');
+const NotFound = require('../errors/NotFound');
 
 class UserService {
-  constructor() {
-    this.userRepository = UserRepository;
+  async getById({ id }) {
+    const tablePK = PKConcatenator.concat('USER', id);
+    const user = await UserRepository.findById(tablePK);
+
+    if (Object.keys(user).length === 0) throw new NotFound('User');
+
+    return user;
   }
 }
 module.exports = new UserService();
