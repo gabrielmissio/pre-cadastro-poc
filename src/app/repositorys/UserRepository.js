732216237f511fe0150e6d9c43c73c1caab34d9a
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-10 17:07:12 -0300

    implement findById method of UserRepository

--- src/app/repositorys/UserRepository.js
+++ src/app/repositorys/UserRepository.js
@@ -1,9 +1,21 @@
-const dynamoDocClient = require('../../infra/config/aws-resources');
+const {dynamoDocClient} = require('../../infra/config/aws-resources')
 
 class UserRepository {
   constructor(tableName = 'Register', sortKey = 'PROFILE') {
     this.tableName = tableName;
     this.sortKey = sortKey;
   }
+
+  async findById(id) {
+    return dynamoDocClient
+      .get({
+        TableName: this.tableName,
+        Key: {
+          PK: id,
+          SK: this.sortKey
+        }
+      })
+      .promise();
+  }
 }
-module.exports = UserRepository;
+module.exports = new UserRepository();
