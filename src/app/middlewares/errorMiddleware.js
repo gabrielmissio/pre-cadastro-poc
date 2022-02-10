8110f839ee441a62a7b42ef042590249339239a4
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-10 15:04:48 -0300

    fix: add next param to error handler method

--- src/app/middlewares/errorMiddleware.js
+++ src/app/middlewares/errorMiddleware.js
@@ -1,13 +1,14 @@
 const NotFound = require('../errors/NotFound');
 const errorSerialize = require('../serialize/errorSerialize');
 
-const errorHandler = (error, req, res) => {
+const errorHandler = (error, req, res, next) => {
   let statusCode = 500;
 
   if (error instanceof NotFound) statusCode = 404;
   else statusCode = 400;
 
   res.status(statusCode).json(errorSerialize(error));
+  next();
 };
 
 module.exports = errorHandler;
