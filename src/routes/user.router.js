b6f243744301f38296ef6b0313ecc3527d07c5c5
Leonardo Guimaraes Moraes <leonardo32moraes@hotmail.com>
2022-02-10 17:46:36 -0300

    feat: add the post method in user.router

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,3 +1,7 @@
+const UserController = require('../app/controllers/UserController');
+const errorHandler = require('../app/middlewares/errorMiddleware');
+
 module.exports = (server, routes, prefix = '/api/v1/user') => {
+  routes.post('/', UserController.create, errorHandler);
   server.use(prefix, routes);
 };
