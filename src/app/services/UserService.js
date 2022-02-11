3d7da6ff3e77eb34db60ac22e07d1a5d9dc79b53
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 13:59:05 -0300

    feat: import concat method in UserService

--- src/app/services/UserService.js
+++ src/app/services/UserService.js
@@ -1,10 +1,11 @@
 const UserRepository = require('../repositorys/UserRepository');
-const PKConcatenator = require('../utils/PK-concatenator');
+const concat = require('../utils/PK-concatenator');
 const NotFound = require('../errors/NotFound');
 
 class UserService {
   async getById({ id }) {
-    const tablePK = PKConcatenator.concat('USER', id);
+    const tablePK = concat('USER', id);
+    console.log('tablePK', tablePK);
     const user = await UserRepository.findById(tablePK);
 
     if (Object.keys(user).length === 0) throw new NotFound('User');
