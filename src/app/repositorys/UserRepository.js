9f359c15003ec9afb4d4687d9510708d7138ab3e
Leonardo Guimaraes Moraes <leonardo32moraes@hotmail.com>
2022-02-11 10:30:36 -0300

    feat: correction of dependency for generating UUID and replacing packages

--- src/app/repositorys/UserRepository.js
+++ src/app/repositorys/UserRepository.js
@@ -1,18 +1,20 @@
-const { v4: uuidv4 } = require('uuid');
+const { randomUUID } = require('crypto');
 const { dynamoDocClient } = require('../../infra/config/aws-resources');
 const PKConcatenator = require('../utils/PK-concatenator');
 
 class UserRepository {
-  constructor(tableName = 'Register', sortKey = 'PROFILE') {
+  constructor(tableName = 'Register', sortKey = 'PROFILE', user = 'USER', uuid = randomUUID()) {
     this.tableName = tableName;
     this.sortKey = sortKey;
+    this.user = user;
+    this.uuid = uuid;
   }
 
   async create(payload) {
-    const v4 = uuidv4();
     const user = 'USER';
-    const userId = PKConcatenator.concat(user, v4);
+    const userId = PKConcatenator.concat(user, this.uuid);
     const dateTime = new Date().toISOString();
+
     const data = {
       PK: userId,
       SK: this.sortKey,
