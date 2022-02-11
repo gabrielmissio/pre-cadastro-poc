78001180ad21c8a597827a8f137a647e661bb0a9
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 13:46:13 -0300

    feat(repositories): remove constructor from UserRepository

--- src/app/repositorys/UserRepository.js
+++ src/app/repositorys/UserRepository.js
@@ -1,18 +1,13 @@
 const { dynamoDocClient } = require('../../infra/config/aws-resources');
 
 class UserRepository {
-  constructor(tableName = 'Register', sortKey = 'PROFILE') {
-    this.tableName = tableName;
-    this.sortKey = sortKey;
-  }
-
   async findById(id) {
     return dynamoDocClient
       .get({
         TableName: this.tableName,
         Key: {
           PK: id,
-          SK: this.sortKey
+          SK: 'PROFILE'
         }
       })
       .promise();
