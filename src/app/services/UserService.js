84c81f453b2c09877fb5de1d0a397f81631b1a80
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 17:21:14 -0300

    fix: remove console.log from getById method of UserService

--- src/app/services/UserService.js
+++ src/app/services/UserService.js
@@ -5,7 +5,6 @@ const NotFound = require('../errors/NotFound');
 class UserService {
   async getById({ id }) {
     const tablePK = concat('USER', id);
-    console.log('tablePK', tablePK);
     const user = await UserRepository.findById(tablePK);
 
     if (Object.keys(user).length === 0) throw new NotFound('User');
