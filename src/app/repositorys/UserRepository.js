6c4652cc430ad06ca22874aa0ece02d69bbdfd4c
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 13:51:32 -0300

    feat: insert enviroment variable AWS_DYNAMO_TABLENAME in TableName field of findById method

--- src/app/repositorys/UserRepository.js
+++ src/app/repositorys/UserRepository.js
@@ -1,10 +1,13 @@
+const dotenv = require('dotenv');
 const { dynamoDocClient } = require('../../infra/config/aws-resources');
+
+dotenv.config();
 
 class UserRepository {
   async findById(id) {
     return dynamoDocClient
       .get({
-        TableName: this.tableName,
+        TableName: process.env.AWS_DYNAMO_TABLENAME,
         Key: {
           PK: id,
           SK: 'PROFILE'
