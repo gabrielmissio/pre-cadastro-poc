f28a94f45c406ee5c7b7b180fadb284ca7232ca5
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 17:37:04 -0300

    feat: create ErrorResponseSerialize class with serialize method

---
+++ src/app/serialize/ErrorResponseSerialize.js
@@ -0,0 +1,16 @@
+class ErrorResponseSerialize {
+  static serialize(error) {
+    if (error.details) {
+      const errors = error.details.map((err) => ({
+        description: err.path[0],
+        name: err.message
+      }));
+
+      return errors;
+    }
+
+    return { description: error.name, name: error.message };
+  }
+}
+
+module.exports = ErrorResponseSerialize.serialize;
