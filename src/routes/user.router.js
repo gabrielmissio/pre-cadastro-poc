1c70950104003cebfe8f3daa0325bb49bb6f860f
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-14 10:43:33 -0300

    feat: add endpoint to delete a user

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,5 +2,6 @@ const userRouter = require('express').Router();
 const UserController = require('../app/controllers/UserController');
 
 userRouter.get('/:id', UserController.getById);
+userRouter.delete('/:id', UserController.delete);
 
 module.exports = userRouter;
